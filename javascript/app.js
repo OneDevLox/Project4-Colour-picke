648c83622d3b4b5f3a9d62e7aaecd6b7fe848831
OneDevLox <wadwool1@gmail.com>
2026-03-22 13:27:20 +0000

    Inital commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,83 +1,54 @@
-const todoForm = document.querySelector("form");
-const todoInput = document.getElementById("todo-input");
-const todoListUL = document.getElementById("todo-list");
+const dropdown = document.getElementById("option");
+const angle = document.getElementById("angle");
 
-let allTodos = getTodos();
-updateTodoList();
+const colorGroups = document.querySelectorAll(".color-group");
 
-todoForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  addTodo();
+const inputColor1 = document.getElementById("color1");
+const inputColor2 = document.getElementById("color2");
+const htmlText = document.getElementById("html-txt")
+
+let color1 = ""
+let color2 = ""
+
+let mode = "solid"
+let angleGrad = ""
+
+dropdown.addEventListener("change", () => {
+    const modes = dropdown.value.toLowerCase();
+    mode = modes
+    if (modes === "linear") {
+        colorGroups[1].classList.add("active");   // show Color 2
+    } else {
+        colorGroups[1].classList.remove("active"); // hide Color 2
+    }
+});
+angle.addEventListener("change", () => {
+    const angleVal = angle.value.toLowerCase();
+    angleGrad = angleVal
+    updateBackground()
 });
 
-function addTodo() {
-  const todoText = todoInput.value.trim();
-  if (todoText.length > 0) {
-    const todoObject = {
-        text: todoText,
-        completed: false,
+inputColor1.addEventListener("change", () => {
+    color1 = inputColor1.value
+    htmlText.innerHTML = `${color1};`
+    updateBackground()
+})
+
+inputColor2.addEventListener("change", () => {
+    color2 = inputColor2.value
+    htmlText.innerHTML = `linear-gradient(${color1}, ${color2});`
+    updateBackground()
+})
+
+
+function updateBackground() {
+    if (mode === "solid") {
+        document.documentElement.style.setProperty("--primary-colour", color1);
+        htmlText.innerHTML = `${color1};`;
+    } else {
+        const gradient = `linear-gradient(${angleGrad},${color1}, ${color2})`;
+        document.documentElement.style.setProperty("--primary-colour", gradient);
+        htmlText.innerHTML = gradient + ";";
     }
-    allTodos.push(todoObject);
-    updateTodoList();
-    saveTodos();
-    todoInput.value = "";
-  }
-}
-function updateTodoList() {
-  todoListUL.innerHTML = "";
-  allTodos.forEach((todo, todoIndex) => {
-    todoItem = createTodoItem(todo, todoIndex);
-    todoListUL.append(todoItem);
-  });
-}
-function createTodoItem(todo, todoIndex) {
-  const todoId = "todo"+todoIndex;
-  const todoLI = document.createElement("li");
-  const todoText = todo.text;
-
-  todoLI.className = "todo";
-  todoLI.innerHTML = `
-    <input type="checkbox" id="${todoId}">
-                <label for="${todoId}" class="custom-checkbox">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                        fill="transparent">
-                        <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
-                    </svg>
-                </label>
-                <label for="${todoId}" class="todo-text">
-                    ${todoText}
-                </label>
-                <button class="delete-button">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                        fill="var(--secondary-color)">
-                        <path
-                            d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
-                    </svg>
-                </button>    
-    `
-    const deleteButton = todoLI.querySelector(".delete-button");
-    deleteButton.addEventListener("click", ()=>{
-        deleteTodoItem(todoIndex);
-    })
-    const checkbox = todoLI.querySelector("input");
-    checkbox.addEventListener("change", ()=>{
-        allTodos[todoIndex].completed = checkbox.checked;
-        saveTodos();
-    })
-    checkbox.checked = todo.completed;
-  return todoLI;
-}
-function deleteTodoItem(todoIndex){
-    allTodos = allTodos.filter((_,i)=> i !== todoIndex);
-    saveTodos();
-    updateTodoList();
 }
 
-function saveTodos(){
-    const todosJson = JSON.stringify(allTodos);
-    localStorage.setItem("todos", todosJson);
-}
-function getTodos(){
-    const todos = localStorage.getItem("todos") || "[]";
-    return JSON.parse(todos);
-}
